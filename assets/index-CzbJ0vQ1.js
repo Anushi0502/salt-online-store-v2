a26fdcacbe0abe10459d96e69c1b4edeecc47e9a
anushi0502 <mittalanushi05@gmail.com>
2026-07-14 18:53:32 +0530

    Resolve Shopify CDN lazy CSS preloads

--- assets/index-CzbJ0vQ1.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CJJ3fygS.js","./index-CV4Cquht.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./salt-entry-47ff5067bc4e.js";import{r as p}from"./index-CV4Cquht.js";const _=p("App",{web:()=>e(()=>import("./web-CJJ3fygS.js"),__vite__mapDeps([0,1])).then(r=>new r.AppWeb)});export{_ as App};
