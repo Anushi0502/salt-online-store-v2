0cbdb99693e75c6c0b44b4e90b20e10d19e826a7
gcs1124 <info@globalcreativeservices.us>
2026-08-11 16:24:46 +0530

    Refresh storefront cards with live ratings

---
+++ assets/MetaPixelTracker-BrKf7OYK.js
@@ -0,0 +1,1 @@
+import{ah as a,r as t}from"./salt-entry-1e31d4d0a8bf.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-CwC_0elD.js";const i=()=>{const e=a();return t.useEffect(()=>r(o),[e.pathname,e.search]),null};export{i as default};
