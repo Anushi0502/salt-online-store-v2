80d21a43dca6630289f8259874e796f95e581549
gcs1124 <info@globalcreativeservices.us>
2026-08-11 19:00:02 +0530

    Publish unified product card design

---
+++ assets/MetaPixelTracker-DK_N1QgB.js
@@ -0,0 +1,1 @@
+import{ah as a,r as t}from"./salt-entry-bcfa73db16ad.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-C67HK0HT.js";const i=()=>{const e=a();return t.useEffect(()=>r(o),[e.pathname,e.search]),null};export{i as default};
