3cd5ba406505b02ccb7effcef061f3476ce23c8a
gcs1124 <info@globalcreativeservices.us>
2026-08-19 12:01:43 +0530

    feat: render Shopify admin navigation in theme

--- assets/MetaPixelTracker-CoEl45Tr.js
+++
@@ -1,1 +0,0 @@
-import{aa as a,r as t}from"./salt-entry-42894d74fcff.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-BpfB8Sv6.js";const i=()=>{const e=a();return t.useEffect(()=>r(o),[e.pathname,e.search]),null};export{i as default};
