ff01b6fe3b06e7ea13eb7c17e7cd84afad753fe8
gcs1124 <info@globalcreativeservices.us>
2026-08-19 15:28:54 +0530

    fix: ship canonical collection navigation

---
+++ assets/MetaPixelTracker-BHfW71Cu.js
@@ -0,0 +1,1 @@
+import{a9 as a,r as t}from"./salt-entry-a2d318e9bf0b.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-j103FKsH.js";const i=()=>{const e=a();return t.useEffect(()=>r(o),[e.pathname,e.search]),null};export{i as default};
