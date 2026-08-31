33ac885a3755589777a45505b2cc895cc9924cb3
gcs1124 <info@globalcreativeservices.us>
2026-08-31 17:42:19 +0530

    preserve noindex on filtered shop routes

---
+++ assets/MetaPixelTracker-5WXqmYr2.js
@@ -0,0 +1,1 @@
+import{a3 as a,r as t}from"./salt-entry-7146021f8087.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-ULLTZVla.js";const i=()=>{const e=a();return t.useEffect(()=>r(o),[e.pathname,e.search]),null};export{i as default};
--- assets/skeleton-Sb6pQQr2.js
+++
@@ -1,1 +0,0 @@
-import{j as t,a}from"./salt-entry-0e3caf9eee62.js";function m({className:e,...s}){return t.jsx("div",{className:a("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
