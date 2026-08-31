a4cb2788cc86348d92e314bbefade56369278c01
gcs1124 <info@globalcreativeservices.us>
2026-08-31 12:20:05 +0530

    sync Shopify theme SEO bundle

--- assets/MetaPixelTracker-BBl-sjjF.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a,r as t}from"./salt-entry-d57c532d1497.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-CjdNQYW3.js";const i=()=>{const e=a();return t.useEffect(()=>r(o),[e.pathname,e.search]),null};export{i as default};
