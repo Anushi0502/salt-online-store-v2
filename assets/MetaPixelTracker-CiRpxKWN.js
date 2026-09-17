bf7299e5aa3d8741a7c98406c211b6e7e007be07
gcs1124 <info@globalcreativeservices.us>
2026-09-17 16:59:50 +0530

    Remove homepage discovery hero

---
+++ assets/MetaPixelTracker-CiRpxKWN.js
@@ -0,0 +1,1 @@
+import{ab as t,r as a}from"./salt-entry-555aaa941a07.js";import{scheduleMetaPixelTask as r,trackMetaPixelPageView as o}from"./meta-pixel-dY3n39eb.js";import"./analytics-events-ClNYvOar.js";const l=()=>{const e=t();return a.useEffect(()=>r(o),[e.pathname,e.search]),null};export{l as default};
---
+++ assets/skeleton-CjEPkwh1.js
@@ -0,0 +1,1 @@
+import{j as t,a}from"./salt-entry-555aaa941a07.js";function m({className:e,...s}){return t.jsx("div",{className:a("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
